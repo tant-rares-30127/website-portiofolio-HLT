7c430653577ebfcdd80374a197bcfb0d1d593a50
delia_popescu <delia.popescu@principal33.com>
2021-09-22 12:11:50 +0300

    setup vue-router, axios, vuex,bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import store from "./store";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from "./App.vue";
+
+
+const app = createApp(App);
+app.use(BootstrapVue)
+app.use(VueAxios, axios);
+app.use(store);
+app.mount('#app')
