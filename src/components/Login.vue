0e1e37c894611f21f4db4122266e80b2f8401cb1
delia_popescu <delia.popescu@principal33.com>
2021-09-22 21:49:14 +0300

    create login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,64 @@
 <template>
-  <div>aici va fi loginpage</div>
+  <div class="wrapper">
+    <div class="fields-wrapper">
+      <div class="in align-items-center">
+        <label class="p-3" for="username"><strong>Username</strong></label>
+        <input type="text" placeholder="Username" style="text-align: center" />
+      </div>
+
+      <div class="in align-items-center">
+        <label class="p-3" for="Password"><strong>Password</strong></label>
+        <input type="text" placeholder="Password" style="text-align: center" />
+      </div>
+      <button class="submit-btn mt-3">Login</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+export default {};
 </script>
 
 <style>
+.wrapper {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: #393e46;
+}
+.fields-wrapper {
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  position: relative;
+  width: 30%;
+  height: 30%;
+  top: 30%;
+  left: 35%;
+  background: #eeeeee;
+  border-radius: 65px;
+}
 
+.fields-wrapper input {
+  width: 50%;
+  height: 60%;
+  top: 20%;
+  background: #aad8d3;
+  opacity: 0.5;
+  border-radius: 50px;
+}
+.submit-btn {
+  position: relative;
+  width: 5rem;
+  height: 15%;
+  left: 40%;
+  background: #00adb5;
+  border-radius: 50px;
+}
+.in {
+   display: flex;
+  flex-direction: column; 
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import router from "./router";
+//import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import store from "./store";
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-import App from "./App.vue";
-
+//import store from "./store";
 
 const app = createApp(App);
-app.use(BootstrapVue)
+
+
 app.use(VueAxios, axios);
-app.use(store);
+//app.use(store);
+
 app.mount('#app')
