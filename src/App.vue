b43deebea12201fa378a303be2947c217c4a1979
TeonaTanasa <teona.tanasa@principal33.com>
2021-09-22 19:06:46 +0300

    select cv page template.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <SelectCV></SelectCV>
+  <Login></Login>
+  <Main></Main>
 </template>
 
 <script>
 import Login from "./components/Login.vue";
 import Main from "./components/Main.vue";
 import SelectCV from "./components/SelectCV.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 export default {
   name: "App",
@@ -17,4 +21,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+:root {
+  --dark-gray: #393e46;
+  --blue: #00adb5;
+  --light-blue: #aad8d3;
+  --light-gray: #eeeeee;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="user-box">
+    <div class="user-info" @click="openCV">
+      <img v-bind:src="photoSrc" alt="profileImage" height="40" />
+      <div class="text-box">
+        <p id="name">{{ name }}</p>
+        <p id="position">{{ position }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id", "name", "position", "photoSrc"],
+  methods: {
+    openCV() {
+      console.log("the user selected has the id: " + this.id);
+    },
+  },
+};
+</script>
+
+<style>
+.user-box {
+  font: roboto;
+  background-color: none;
+  width: 25rem;
+  padding: 1.5rem;
+}
+
+.user-info {
+  font: roboto;
+  background-color: var(--light-gray);
+  width: 23rem;
+  height: 7rem;
+  display: flex;
+  flex-direction: row;
+  border-radius: 30px;
+}
+.user-box img {
+  width: 4.75em;
+  height: 4.75em;
+  margin: 5%;
+  border-radius: 50%;
+}
+
+.text-box {
+  width: 50%;
+  margin: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.text-box p {
+  font-family: Roboto;
+  color: black;
+  text-align: center;
+  line-height: 20px;
+}
+
+.text-box #name {
+  font-weight: bold;
+  font-size: 24px;
+  padding-top: 17%;
+}
+
+.text-box #position {
+  font-weight: normal;
+  font-size: 21px;
+  margin-bottom: 20%;
+}
+</style>
