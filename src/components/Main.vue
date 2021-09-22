48a5d697cba076e38278471d6c9144001bd6b8d9
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-09-22 16:44:30 +0300

    Created, styled the navbar

---
+++ src/components/Main.vue
@@ -0,0 +1,125 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light pt-4">
+    <div class="container-fluid">
+      <div class="navbar-name">
+        <a class="navbar-brand ms-3" id="name" href="#">Rares</a>
+      </div>
+      <div class="d-flex">
+        <div class="collapse navbar-collapse me-3"  id="navbarSupportedContent"  >
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i class="fas fa-bars"></i>
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <div class="dropdown-item-li">
+                  <li><a class="dropdown-item" href="#">Claudia</a></li>
+                  <li><a class="dropdown-item" href="#">Delia</a></li>
+                  <li><a class="dropdown-item" href="#">Rares</a></li>
+                  <li><a class="dropdown-item" href="#">Teona</a></li>
+                </div>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+</template>
+<style>
+.dropdown-menu {
+  right: auto;
+  left: 60%;
+  -webkit-transform: translate(-50%, 0);
+  -o-transform: translate(-50%, 0);
+  transform: translate(-70%, 0);
+}
+#name {
+  font-size: 2rem;
+  color: white;
+}
+
+.navbar-name {
+  border: 3px solid #ffffff;
+  box-sizing: border-box;
+  border-radius: 116px;
+  padding-bottom: 0.5%;
+  padding-top: 0.3%;
+  font-size: 24px;
+}
+
+#navbarDropdown {
+  color: white;
+}
+.d-flex a {
+  font-size: 1.5rem;
+}
+
+.dropdown-item-li a {
+  background: #00adb5;
+  border: 3px solid #00adb5;
+  box-sizing: border-box;
+  border-radius: 116px;
+  display: flex;
+  justify-content: center;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+  color: white;
+}
+
+.dropdown-item-li li {
+  padding-left: 10%;
+  padding-right: 10%;
+  padding-top: 2%;
+  padding-bottom: 2%;
+}
+
+.about-text h2 {
+  padding-right: 80%;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+
+  color: #23262b;
+}
+
+.about-text h3 {
+  color: #23262b;
+
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+}
+
+.about-photo {
+  background-color: grey;
+  padding-right:10%;
+  margin-left:10%;
+  margin-right:10%;
+}
+
+.about {
+  display: flex;
+  justify-content: space-around;
+  padding-top:3%
+}
+
+.navbar {
+  background-color: #23262b;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import router from "./router";
-import axios from "axios";
-import VueAxios from "vue-axios";
-import store from "./store";
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import router from "./router";
+// import axios from "axios";
+// import VueAxios from "vue-axios";
+// import store from "./store";
 
-import App from "./App.vue";
+
+// import App from "./App.vue";
 
 
 const app = createApp(App);
-app.use(BootstrapVue)
-app.use(VueAxios, axios);
-app.use(store);
+// app.use(BootstrapVue)
+// app.use(VueAxios, axios);
+// app.use(store);
 app.mount('#app')
