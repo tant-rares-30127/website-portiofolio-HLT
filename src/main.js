53a6bf5bd1cba9a16c1f9b4493594932e6ca6642
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-09-24 00:48:57 +0300

    Added progress bar, added import

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ProgressBar from 'vuejs-progress-bar'
 
 // import router from "./router";
 // import axios from "axios";
@@ -8,7 +9,7 @@ import App from './App.vue'
 
 
 // import App from "./App.vue";
-
+app.use(ProgressBar)
 
 const app = createApp(App);
 // app.use(BootstrapVue)
