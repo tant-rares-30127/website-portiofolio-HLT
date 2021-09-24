b5730faed42207c5910b42695eb048763778db7f
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-09-24 20:56:29 +0300

    deleted comments

--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import router from "./router";
-// import axios from "axios";
-// import VueAxios from "vue-axios";
-// import store from "./store";
-
-
-// import App from "./App.vue";
-
 const app = createApp(App);
-// app.use(BootstrapVue)
-// app.use(VueAxios, axios);
-// app.use(store);
 app.mount('#app')
