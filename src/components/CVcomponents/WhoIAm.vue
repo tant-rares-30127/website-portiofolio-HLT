9ea63816c5307d729776bd582e7384838cf0fe45
TeonaTanasa <teona.tanasa@principal33.com>
2021-09-26 23:17:31 +0300

    styled the WhoIAm page and added fontAwesome.

--- src/components/CVcomponents/WhoIAm.vue
+++ src/components/CVcomponents/WhoIAm.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="about">
     <div class="about-text">
-    <div class="about-photo">
-      <img :src="imgSrc" alt="buna" />
+      <div class="about-photo">
+        <img :src="imgSrc" alt="buna" height="300" />
+      </div>
+      <h3 class="CVname">{{ user.firstName }} {{ user.lastName }}</h3>
     </div>
-      <h3>{{ user.firstName }} {{ user.lastName }}</h3>
-    </div>
-      <h2>About</h2>
-      <div>{{ introduction }}</div>
-    </div>
-  <div class="about-contact">
-    <h2>Contact</h2>
-    <br />
-    <div class="about-contact-i">
-      <a href=""><i id="first" class="fab fa-linkedin"></i></a>
-      <a href=""><i class="fab fa-github"></i></a>
-      <a href=""><i class="fas fa-phone-square"></i></a>
-      <a href=""><i class="fas fa-envelope"></i></a>
+    <div class="right-part">
+      <div class="introduction-box">
+        <h2>About</h2>
+        <p>{{ introduction }}</p>
+      </div>
+      <div class="about-contact">
+        <h2>Contact</h2>
+        <br />
+        <div class="about-contact-i">
+          <a href="https://www.linkedin.com/"
+            ><i class="fab fa-linkedin"></i
+          ></a>
+          <a href="https://github.com/"><i class="fab fa-github"></i></a>
+          <a href=""><i class="fas fa-phone-square"></i></a>
+          <a href=""><i class="fas fa-envelope"></i></a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,226 +34,45 @@ export default {
 </script>
 
 <style>
-.about,
-.about-contact,
-br {
-  background-color: var(--dark-gray);
-}
-
-.dropdown-menu {
-  right: auto;
-  left: 60%;
-  -webkit-transform: translate(-50%, 0);
-  -o-transform: translate(-50%, 0);
-  transform: translate(-70%, 0);
-}
-
-#name {
-  font-size: 2rem;
-  color: white;
-}
-
-.navbar-name {
-  border: 3px solid #ffffff;
-  box-sizing: border-box;
-  border-radius: 116px;
-  padding-bottom: 0.5%;
-  padding-top: 0.3%;
-  font-size: 24px;
-}
-
-#navbarDropdown {
-  color: white;
-}
-
-.d-flex a {
-  font-size: 1.5rem;
-}
-
-.dropdown-item-li a {
-  background: #00adb5;
-  border: 3px solid #00adb5;
-  box-sizing: border-box;
-  border-radius: 116px;
-  display: flex;
-  justify-content: center;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 28px;
-  text-align: center;
-  color: white;
-}
-
-.dropdown-item-li li {
-  padding-left: 10%;
-  padding-right: 10%;
-  padding-top: 2%;
-  padding-bottom: 2%;
-  background-color: white;
-}
-
-.about-text h2,
-.about-contact h2 {
-  padding-right: 80%;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 28px;
-  text-align: center;
-  color: #ffffff;
-}
-
-.about-contact h2 {
-  padding-right: 85%;
-  padding-top: 3%;
-}
-
-.about-text h3 {
-  color: #ffffff;
-  font-style: normal;
-  font-weight: lighter;
-  font-size: 24px;
-  line-height: 28px;
-  text-align: center;
-  padding-left: 4.7%;
-}
-
-.about-photo {
-  background-color: grey;
-  padding-right: 10%;
-  margin-left: 10%;
-  margin-right: 10%;
-}
-
 .about {
+  margin-top: 1200px;
+  margin-left: 50px;
+  color: white;
   display: flex;
   justify-content: space-around;
   padding-top: 3%;
 }
 
-.navbar {
-  background-color: var(--dark-gray);
+.about-photo img {
+  border-radius: 30px;
+  left: 50px;
 }
 
-.about-contact a {
-  font-size: 3rem;
-  padding-left: 4.7%;
-  color: black;
-}
-
-.about-contact i:hover {
-  color: #00adb5;
-}
-.about,
-.about-contact,
-br {
-  background-color: var(--dark-gray);
-}
-
-.dropdown-menu {
-  right: auto;
-  left: 60%;
-  -webkit-transform: translate(-50%, 0);
-  -o-transform: translate(-50%, 0);
-  transform: translate(-70%, 0);
-}
-
-#name {
+.CVname {
+  margin-left: 30px;
   font-size: 2rem;
-  color: white;
 }
 
-.navbar-name {
-  border: 3px solid #ffffff;
-  box-sizing: border-box;
-  border-radius: 116px;
-  padding-bottom: 0.5%;
-  padding-top: 0.3%;
-  font-size: 24px;
-}
-
-#navbarDropdown {
-  color: white;
-}
-
-.d-flex a {
-  font-size: 1.5rem;
-}
-
-.dropdown-item-li a {
-  background: #00adb5;
-  border: 3px solid #00adb5;
-  box-sizing: border-box;
-  border-radius: 116px;
-  display: flex;
-  justify-content: center;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 28px;
-  text-align: center;
-  color: white;
-}
-
-.dropdown-item-li li {
-  padding-left: 10%;
-  padding-right: 10%;
-  padding-top: 2%;
-  padding-bottom: 2%;
-  background-color: white;
-}
-
-.about-text h2,
 .about-contact h2 {
-  padding-right: 80%;
+  padding-right: 100%;
+  padding-top: 3%;
+  color: #ffffff;
   font-style: normal;
   font-weight: bold;
   font-size: 24px;
   line-height: 28px;
   text-align: center;
   color: #ffffff;
-}
-
-.about-contact h2 {
-  padding-right: 85%;
-  padding-top: 3%;
-}
-
-.about-text h3 {
-  color: #ffffff;
-  font-style: normal;
-  font-weight: lighter;
-  font-size: 24px;
-  line-height: 28px;
-  text-align: center;
-  padding-left: 4.7%;
-}
-
-.about-photo {
-  background-color: grey;
-  padding-right: 10%;
-  margin-left: 10%;
-  margin-right: 10%;
-}
-
-.about {
-  display: flex;
-  justify-content: space-around;
-  padding-top: 3%;
-}
-
-.navbar {
-  background-color: var(--dark-gray);
 }
 
 .about-contact a {
   font-size: 3rem;
-  padding-left: 4.7%;
+  padding-left: 2.2%;
   color: black;
 }
-
-.about-contact i:hover {
+.about-contact a:hover {
   color: #00adb5;
+}
+.about-contact-i {
 }
 </style>
